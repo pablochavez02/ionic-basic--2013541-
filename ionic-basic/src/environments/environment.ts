6eabdcd7e4638a5cba76520478f9854fcc9f8080
pablochavez02 <141696292+pablochavez02@users.noreply.github.com>
2023-11-18 21:28:08 -0600

    practica 12

--- ionic-basic/src/environments/environment.ts
+++ ionic-basic/src/environments/environment.ts
@@ -12,7 +12,8 @@ export const environment = {
     messagingSenderId: "256850523052",
     appId: "1:256850523052:web:308823818ef1c6e87b4009",
     measurementId: "G-BR9MT011GY"
-  }
+  },
+  apiKeyMaps: 'AIzaSyC1m8Y_hgzWsXzq8GGa8aHOVogBzMW9z7c'
 };
 
 /*
